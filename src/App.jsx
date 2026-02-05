e41f226e6c9caf32e23bc57ef20e7ef20f407861
Vlad <vlad086367@gmail.com>
2026-02-05 21:30:35 +0200

    Add task React Redux Intro

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import TodoApp from './pages/TodoAppPages';
+import CardUser from './pages/CardUser';
 
 function App () {
   return (
     <>
-      <TodoApp />
+      {/* <TodoApp /> */}
+      <CardUser />
     </>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from './slices/todoSlice';
+import userReduser from './slices/userSlice';
 
 const store = configureStore({
   reducer: {
     todoTask: todoReducer,
+    userCard: userReduser,
   },
 });
 
